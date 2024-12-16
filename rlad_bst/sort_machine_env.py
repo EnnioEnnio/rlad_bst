72398d54fdcb61b634f0acfeaf948dbbc85f649f
FSadrieh <Frederic.Sadrieh@student.hpi.uni-potsdam.de>
2024-12-16 11:42:27 +0100

    Add wandb to PPO

--- rlad_bst/sort_machine_env.py
+++ rlad_bst/sort_machine_env.py
@@ -6,7 +6,10 @@ from gymnasium import spaces
 from gymnasium.envs.registration import register
 from gymnasium.utils.env_checker import check_env
 from stable_baselines3 import PPO
+from stable_baselines3.common.monitor import Monitor
+from wandb.integration.sb3 import WandbCallback
 
+import wandb
 from rlad_bst.reward import calculate_reward
 
 register(id="rlad/bst-v0", entry_point="sort_machine_env:SortingMachine")
@@ -35,26 +38,24 @@ class SortingMachine(gym.Env):
             1: self.left,
             2: self.push,
             3: self.pop,
-            4: self.compare,
-            5: self.mark,
-            6: self.jump,
-            7: self.swap,
-            8: self.isnotend,
-            9: self.isnotstart,
-            10: self.isnotequal,
-            11: self.drop,
-            12: self.swapright,
-            13: self.compareright,
-            14: self.leftchild,
-            15: self.rightchild,
-            16: self.parent,
-            17: self.leftchildempty,
-            18: self.rightchildempty,
-            19: self.write,
-            20: self.nodeempty,
-            21: self.istreeend,
-            22: self.isnottreestart,
-            23: self.giveresult,
+            4: self.mark,
+            5: self.jump,
+            6: self.isnotend,
+            7: self.isnotstart,
+            8: self.isnotequal,
+            9: self.drop,
+            10: self.swapright,
+            11: self.compareright,
+            12: self.leftchild,
+            13: self.rightchild,
+            14: self.parent,
+            15: self.leftchildempty,
+            16: self.rightchildempty,
+            17: self.write,
+            18: self.nodeempty,
+            19: self.istreeend,
+            20: self.isnottreestart,
+            # 23: self.giveresult,
         }
 
         self.action_space = spaces.Discrete(len(self._action_to_command))
@@ -73,30 +74,33 @@ class SortingMachine(gym.Env):
                     low=0, high=np.inf, shape=(data_len,), dtype=np.int64
                 ),  # np.array of size data_len
                 "pointers": spaces.Box(
-                    low=0, high=data_len, shape=(data_len,), dtype=np.int64
+                    low=0, high=data_len + 1, shape=(data_len,), dtype=np.int64
                 ),  # np.array of size data_len
                 "stack": spaces.Box(
-                    low=0, high=program_len, shape=(data_len,), dtype=np.int64
+                    low=0,
+                    high=program_len + 1,
+                    shape=(data_len,),
+                    dtype=np.int64,
                 ),  # np.array of size data_len
                 "skipflag": spaces.Discrete(2),  # np.int64 (0 or 1)
-                "commandpointer": spaces.Discrete(program_len),  # np.int64
+                "commandpointer": spaces.Discrete(program_len + 1),  # np.int64
                 "lastcommand": spaces.Discrete(
                     len(self._action_to_command) + 1
                 ),  # np.int64
-                "lastconditional": spaces.Discrete(
-                    3
-                ),  # np.int64 None (2), True (1), or False (0)
+                # "lastconditional": spaces.Discrete(
+                #     3
+                # ),  # np.int64 None (2), True (1), or False (0)
                 "execcost": spaces.Box(
                     low=0, high=np.inf, shape=(1,), dtype=np.int64
                 ),  # np.int64
-                "storage": spaces.Box(
-                    low=0, high=np.inf, shape=(1,), dtype=np.int64
-                ),  # np.int64
+                # "storage": spaces.Box(
+                #     low=0, high=np.inf, shape=(1,), dtype=np.int64
+                # ),  # np.int64
                 "result": spaces.Box(
                     low=0, high=np.inf, shape=(data_len,), dtype=np.int64
                 ),  # np.array of size data_len
                 "pointersresult": spaces.Box(
-                    low=0, high=data_len, shape=(data_len,), dtype=np.int64
+                    low=0, high=data_len + 1, shape=(data_len,), dtype=np.int64
                 ),  # np.array of size data_len
             }
         )
@@ -106,16 +110,16 @@ class SortingMachine(gym.Env):
 
     def _initial_machine_state(self):
         self.program: np.array = np.array([], dtype=np.int64)
-        self.data: np.array = np.array(list(range(self.data_len)))
+        self.data: np.array = np.array(list(range(1, self.data_len + 1)))
         self.pointers: list = [0]
         self.stack: list = []
         self.skipflag: bool = False
         self.commandpointer: int = 0
         self.lastcommand: int = len(self._action_to_command)
-        self.lastconditional: int = 2
+        # self.lastconditional: int = 2
         self.execcost: int = 0
-        self.storage: int = self.pad
-        self.result: np.array = np.zeros((len(self.data)), dtype=int)
+        # self.storage: int = self.pad
+        self.result: np.array = np.ones_like(self.data, dtype=int) * self.pad
         self.pointersresult: list = [0]
 
     def _make_binary_tree(self):
@@ -170,9 +174,9 @@ class SortingMachine(gym.Env):
             "skipflag": int(self.skipflag),
             "commandpointer": self.commandpointer,
             "lastcommand": self.lastcommand,
-            "lastconditional": self.lastconditional,
+            # "lastconditional": self.lastconditional,
             "execcost": np.array([self.execcost]),
-            "storage": np.array([self.storage]),
+            # "storage": np.array([self.storage]),
             "result": self.result,
             "pointersresult": pointersresult,
         }
@@ -188,9 +192,12 @@ class SortingMachine(gym.Env):
         return self._get_obs(), self._get_info()
 
     def _check_terminated(self):
+        # return np.array_equal(self.data, self.correct_tree)
+        return np.array_equal(self.result, self.correct_tree)
+
+    def _check_trucated(self):
         return (
-            np.array_equal(self.data, self.correct_tree)
-            or self.commandpointer == self.program_len
+            self.commandpointer >= self.program_len
             or self.execcost == self.maximum_exec_cost
         )
 
@@ -205,19 +212,23 @@ class SortingMachine(gym.Env):
         2. The machine awaits a new instruction
         """
         self.program = np.append(self.program, action)
+        terminated = self._check_terminated()
+        truncated = self._check_trucated()
         while not (
-            self._check_terminated()
-            or self.commandpointer >= len(self.program)
+            terminated or truncated or self.commandpointer >= len(self.program)
         ):
             self._transition_once()
             if self.verbosity >= 1:
                 self.render()
+            terminated = self._check_terminated()
+            truncated = self._check_trucated()
 
-        reward = calculate_reward(
-            solution_arr=self.correct_tree, candidate_arr=self.data
+        reward = (
+            calculate_reward(
+                solution_arr=self.correct_tree, candidate_arr=self.result
+            )
+            * self.execcost
         )
-        terminated = self._check_terminated()
-        truncated = False  # Used to limit steps
 
         return (
             self._get_obs(),
@@ -232,7 +243,7 @@ class SortingMachine(gym.Env):
         self.lastcommand = cmd
         self.commandpointer += 1
 
-        self.lastconditional = 2  # Set to None
+        # self.lastconditional = 2  # Set to None
         if self.skipflag:
             self.skipflag = False
         else:
@@ -248,7 +259,7 @@ class SortingMachine(gym.Env):
         print("skipflag: ", self.skipflag)
         print("commandpointer: ", self.commandpointer)
         print("lastcommand: ", self.lastcommand)
-        print("lastconditional: ", self.lastconditional)
+        # print("lastconditional: ", self.lastconditional)
         print("execcost: ", self.execcost)
         print("---")
 
@@ -275,7 +286,6 @@ class SortingMachine(gym.Env):
             self.pointers[-1] -= 1
 
     def push(self):
-        print("push:", self.pointersresult[-1])
         if len(self.pointersresult) < self.data_len - 1:
             self.pointersresult.append(self.pointersresult[-1])
 
@@ -286,22 +296,21 @@ class SortingMachine(gym.Env):
     def pointernotempty(self):
         self.skipflag = len(self.pointers) <= 1
 
-    def compare(self):
-        self.skipflag = self.data[self.pointers[-1]] <= self.storage
+    # def compare(self):
+    #     self.skipflag = self.data[self.pointers[-1]] <= self.storage
 
     def mark(self):
         if len(self.stack) < self.data_len - 1:
             self.stack.append(self.commandpointer - 1)
 
     def jump(self):
-        print("jump")
         if len(self.stack) != 0:
             self.commandpointer = self.stack.pop()
 
-    def swap(self):
-        temp = self.storage
-        self.storage = self.data[self.pointers[-1]]
-        self.data[self.pointers[-1]] = temp
+    # def swap(self):
+    #     temp = self.storage
+    #     self.storage = self.data[self.pointers[-1]]
+    #     self.data[self.pointers[-1]] = temp
 
     def isnotend(self):
         self.skipflag = self.pointers[-1] > len(self.data) - 1
@@ -340,13 +349,11 @@ class SortingMachine(gym.Env):
 
     def leftchild(self):
         left_child = self.pointersresult[-1] * 2 + 1
-        print("left")
         if left_child < len(self.data):
             self.pointersresult[-1] = left_child
 
     def rightchild(self):
         right_child = self.pointersresult[-1] * 2 + 2
-        print("right")
         if right_child < len(self.data):
             self.pointersresult[-1] = right_child
 
@@ -355,7 +362,6 @@ class SortingMachine(gym.Env):
         if left_child >= len(self.data):
             self.skipflag = 1
         else:
-            print("leftempty", left_child)
             self.skipflag = self.result[left_child] != 0
 
     def rightchildempty(self):
@@ -363,19 +369,17 @@ class SortingMachine(gym.Env):
         if right_child >= len(self.data):
             self.skipflag = 1
         else:
-            print("rightempty", right_child)
+
             self.skipflag = self.result[right_child] != 0
 
     def nodeempty(self):
         self.skipflag = self.result[self.pointersresult[-1]] != 0
 
     def parent(self):
-        print("parent", int((self.pointersresult[-1] - 1) / 2))
         if self.pointersresult[-1] != 0:
             self.pointersresult[-1] = int((self.pointersresult[-1] - 1) / 2)
 
     def write(self):
-        print("write", self.data[self.pointers[-1]])
         self.result[self.pointersresult[-1]] = self.data[self.pointers[-1]]
         self.right()
 
@@ -383,15 +387,14 @@ class SortingMachine(gym.Env):
         self.skipflag = self.pointersresult[-1] * 2 + 1 <= len(self.data) - 1
 
     def isnottreestart(self):
-        print(self.result)
-        print("isnottreestart", self.pointersresult[-1])
         self.skipflag = self.pointersresult[-1] == 0
 
-    def giveresult(self):
-        print("here")
-        self.data = self.result
+    # def giveresult(self):
+    #     self.data = self.result
 
 
+# TODO: Would be nice in a different file:
+# 1 file for env, 1 for training, 1 for config
 if __name__ == "__main__":
     # Test env
     def wait_for_debugger(port: int = 5678):
@@ -406,29 +409,72 @@ if __name__ == "__main__":
         print(f"Waiting for client to attach on port {port}... ")
         debugpy.wait_for_client()
 
-    # wait_for_debugger()
+    # TODO: Make args
+    config = {
+        "data_len": 7,
+        "program_len": 64,  # Ours needs 51
+        "maximum_exec_cost": 128,  # Ours needs for 7 data points 82
+        "verbosity": 0,
+        "total_timesteps": 250000,
+        "gradient_save_freq": 100,
+        "offline": True,
+        "debug": False,
+        "model_checkpoint": None,  # "models/x1vzt59t/model.zip"
+    }
+    if config["debug"]:
+        wait_for_debugger()
+
+    if config["offline"]:
+        import os
+
+        os.environ["WANDB_MODE"] = "dryrun"
 
     env = gym.make(
         "rlad/bst-v0",
         render_mode="human",
-        data_len=10,
-        program_len=100,
-        maximum_exec_cost=100,
-        verbosity=0,
+        data_len=config["data_len"],
+        program_len=config["program_len"],
+        maximum_exec_cost=config["maximum_exec_cost"],
+        verbosity=config["verbosity"],
     )
 
     check_env(env.unwrapped)
 
-    model = PPO("MultiInputPolicy", env, verbose=1)
-    model.learn(total_timesteps=10, progress_bar=True)
+    if not config["model_checkpoint"]:
+        run = wandb.init(
+            project="sb3",
+            config=config,
+            sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+            monitor_gym=True,
+            save_code=True,
+        )
+        env = Monitor(env)
 
-    print("Done")
-    # TODO: Does not make much sense before we know our model can learn
-    # vec_env = model.get_env()
-    # obs = vec_env.reset()
-    # for _ in range(1000):
-    #     action, _states = model.predict(obs, deterministic=True)
-    #     obs, reward, done, info = vec_env.step(action)
-    #     vec_env.render()
+        model = PPO(
+            "MultiInputPolicy",
+            env,
+            verbose=config["verbosity"],
+            tensorboard_log=f"runs/{run.id}",
+        )
+        model.learn(
+            total_timesteps=config["total_timesteps"],
+            callback=WandbCallback(
+                gradient_save_freq=config["gradient_save_freq"],
+                model_save_path=f"models/{run.id}",
+                verbose=config["verbosity"],
+            ),
+        )
 
-    env.close()
+        run.finish()
+
+    else:
+        model = PPO.load(config["model_checkpoint"])
+        obs, info = env.reset()
+        terminated, truncated = False, False
+        while not terminated and not truncated:
+            action, _states = model.predict(obs)
+            obs, reward, terminated, truncated, info = env.step(action)
+            env.render()
+        print(obs["program"])
+
+        env.close()
